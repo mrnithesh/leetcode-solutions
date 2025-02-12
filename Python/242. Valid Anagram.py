fdd459358442adad609564f75eaa2a379a684bae
mrnithesh <mr.nithesh.k@gmail.com>
2025-02-12 12:55:40 +0530

    242. Valid Anagram

---
+++ Python/242. Valid Anagram.py
@@ -0,0 +1,7 @@
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        list_s=[x for x in s]
+        list_t=[x for x in t]
+        list_s.sort()
+        list_t.sort()
+        return  list_s==list_t
