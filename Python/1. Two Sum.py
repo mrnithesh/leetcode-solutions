8d00496fd0a45673a4684a720c5ab0f40cfec59e
mrnithesh <mr.nithesh.k@gmail.com>
2025-02-24 18:49:46 +0530

    Add hashmap approach for Two Sum problem solution

--- Python/1. Two Sum.py
+++ Python/1. Two Sum.py
@@ -6,3 +6,14 @@ class Solution:
                 if (nums[j]==target-nums[i]):
                     return [i,j]
         return [i,j]
+
+        #hashmap approach
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        hashmap={}
+        for i, num in enumerate(nums):
+            diff=target-nums[i]
+            if diff in hashmap:
+                return[i,hashmap[diff]]
+            hashmap[num]=i 
+        
+        return []
