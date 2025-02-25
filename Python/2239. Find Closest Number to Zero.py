af488515f72158eaee86d96aa5488b2c90f531a5
mrnithesh <mr.nithesh.k@gmail.com>
2025-02-25 14:57:50 +0530

    Revert "2239. handles edge cases"

--- Python/2239. Find Closest Number to Zero.py
+++ Python/2239. Find Closest Number to Zero.py
@@ -2,6 +2,6 @@ class Solution:
     def findClosestNumber(self, nums: List[int]) -> int:
         closest=nums[0]
         for i in nums:
-            if  abs(i)< abs(closest) or abs(i)==abs(closest) and i>closest :
+            if  abs(i)< abs(closest) or i==abs(closest):
                 closest=i
         return closest
