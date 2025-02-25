39c14fdeedcddeb58d87735a990f136a190a55db
mrnithesh <mr.nithesh.k@gmail.com>
2025-02-25 21:33:04 +0530

    Add alternative solution for Palindrome Number without string conversion

--- Python/9. Palindrome Number.py
+++ Python/9. Palindrome Number.py
@@ -2,3 +2,12 @@ class Solution:
     def isPalindrome(self, x: int) -> bool:
         str_x=str(x)
         return (str_x[::-1]==str(x))
+
+    #without converting the integer to a string    
+    def isPalindrome(self, x: int) -> bool:
+        og=x
+        rev=0
+        while x>0:
+            rev=rev*10 + x%10
+            x//=10
+        return og==rev
