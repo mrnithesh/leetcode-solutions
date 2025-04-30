77ea294ec91626a0f522d94e13154125f51dc7a1
mrnithesh <mr.nithesh.k@gmail.com>
2025-04-30 13:27:30 +0530

    Add mathematical approach to find numbers with even number of digits

--- Python/1295. Find Numbers with Even Number of Digits.py
+++ Python/1295. Find Numbers with Even Number of Digits.py
@@ -5,3 +5,16 @@ class Solution:
             if len(str(num)) % 2 == 0:
                 count += 1
         return count 
+
+#mathematicall approach
+class Solution:
+    def findNumbers(self, nums: List[int]) -> int:
+        count = 0
+        for num in nums:
+            temp=0
+            while num>0:
+                temp += 1
+                num //= 10
+            if temp %2 ==0:
+                count +=1
+        return count 
