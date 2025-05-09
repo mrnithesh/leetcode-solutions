7919b3c6837f317699639992e61ad2251e00343d
mrnithesh <mr.nithesh.k@gmail.com>
2025-05-09 16:09:15 +0530

    Add solution for 29. Divide Two Integers problem

---
+++ Python/29. Divide Two Integers.py
@@ -0,0 +1,18 @@
+class Solution:
+    def divide(self, dividend: int, divisor: int) -> int:
+        if dividend == -2**31 and divisor == -1:
+            return 2**31 - 1
+
+        sign = -1 if (dividend < 0) != (divisor < 0) else 1
+        dividend, divisor = abs(dividend), abs(divisor)
+        
+        quotient = 0
+        while dividend >= divisor:
+            temp, multiple = divisor, 1
+            while dividend >= (temp << 1):
+                temp <<= 1
+                multiple <<= 1
+            dividend -= temp
+            quotient += multiple
+
+        return sign * quotient
