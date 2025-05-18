e3dae5f05707d9df2eaf96e85be6078e2f4ffd1b
mrnithesh <mr.nithesh.k@gmail.com>
2025-05-18 12:25:42 +0530

    Refactor solution for 189. Rotate Array problem

--- Python/189. Rotate Array.py
+++ Python/189. Rotate Array.py
@@ -11,3 +11,19 @@ class Solution:
                 temp[(i+k)%len(nums)] = nums[i]
         for i in range(len(nums)):
             nums[i] = temp[i]
+
+
+class Solution: #O(1) SC : no addtional memory space
+    def rotate(self, nums: List[int], k: int) -> None:
+        """
+        Do not return anything, modify nums in-place instead.
+        """
+        k = k % len(nums)
+        def reverse(l,r):
+            while l<r:
+                nums[l] , nums[r] = nums[r], nums[l]
+                l += 1
+                r -= 1
+        reverse(0,len(nums)-1)
+        reverse(0,k-1)
+        reverse(k,len(nums)-1) 
