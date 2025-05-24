d566c2fa0571810b7f8c6ca314c852859c7386a4
mrnithesh <mr.nithesh.k@gmail.com>
2025-05-24 17:37:14 +0530

    Refactor solution to implement custom quicksort for finding target indices

--- Python/2089. Find Target Indices After Sorting Array.py
+++ Python/2089. Find Target Indices After Sorting Array.py
@@ -6,3 +6,22 @@ class Solution:
             if nums[i] == target:
                 res.append(i)
         return res
+    
+class Solution:
+    def targetIndices(self, nums: List[int], target: int) -> List[int]:
+        def quickSort(arr): #own sorting implementtation- quicksort
+            if len(arr)<=1:
+                return arr
+            pivot = arr[len(arr)//2]
+
+            left = [x for x in arr if x<pivot]
+            middle =[x for x in arr if x==pivot]
+            right =[x for x in arr if x>pivot]
+
+            return quickSort(left)+middle+quickSort(right)
+        nums=quickSort(nums)
+        res = []
+        for i in range(len(nums)):
+            if nums[i] == target:
+                res.append(i)
+        return res
