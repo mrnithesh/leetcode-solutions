d2002cb2a5843aa4bb695385e8e1def0db610df1
mrnithesh <mr.nithesh.k@gmail.com>
2025-06-22 20:33:18 +0530

    Refactor 287. findDuplicate

--- Python/287. Find the Duplicate Number.py
+++ Python/287. Find the Duplicate Number.py
@@ -1,6 +1,18 @@
-class Solution:
+class Solution: #O(n) - SC
     def findDuplicate(self, nums: List[int]) -> int:
         hashmap = defaultdict(int)
         for i in nums:
             hashmap[i] +=1
         return [x for x,y in hashmap.items() if y>=2][0]
+
+class Solution: #O(n) - SC
+    def findDuplicate(self, nums: List[int]) -> int:
+        numSet = set()
+        res = None
+        for i in nums:
+            if i not in numSet:
+                numSet.add(i)
+            elif i in numSet:
+                res=i
+        return res
+
