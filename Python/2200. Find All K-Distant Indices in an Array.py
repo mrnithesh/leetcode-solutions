0c6f81ebc9b6a41950ccb2033abe579f9622362b
mrnithesh <mr.nithesh.k@gmail.com>
2025-06-24 11:33:07 +0530

    Refactor findKDistantIndices method

--- Python/2200. Find All K-Distant Indices in an Array.py
+++ Python/2200. Find All K-Distant Indices in an Array.py
@@ -12,3 +12,16 @@ class Solution:
                 if abs(i-keyindx) <= k:
                     res.add(i)
         return list(res)
+    
+
+class Solution: #O(N) - TC
+    def findKDistantIndices(self, nums: List[int], key: int, k: int) -> List[int]:
+        res = set()        
+        for i in range(len(nums)):
+            if nums[i] == key:
+                start = max(0, i - k)
+                end = min(len(nums) - 1, i + k)
+                for j in range(start, end + 1):
+                    res.add(j)
+        
+        return sorted(res) 
