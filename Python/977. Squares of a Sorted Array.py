3ccec78787eb3914c1bc99159920110d42d5594e
mrnithesh <mr.nithesh.k@gmail.com>
2025-07-04 10:32:06 +0530

    Refactor sortedSquares implementation to use two-pointer technique

--- Python/977. Squares of a Sorted Array.py
+++ Python/977. Squares of a Sorted Array.py
@@ -1,4 +1,14 @@
 class Solution:
     def sortedSquares(self, nums: List[int]) -> List[int]:
-        nums=[x**2 for x in nums]
-        return sorted(nums) # O(nlogn) complexity
+        l = 0
+        r = len(nums)-1
+        res = [0]*len(nums)
+        for i in range(len(nums)-1,-1,-1):
+            if abs(nums[l]) > abs(nums[r]):
+                res[i] = nums[l] **2
+                l+=1
+            else:
+                res[i] = nums[r]**2
+                r-=1
+        return res
+                
