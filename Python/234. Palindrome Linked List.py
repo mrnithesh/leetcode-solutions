c1f351b82fd82c45bc4ec35c78f7bb06e981e6e9
mrnithesh <mr.nithesh.k@gmail.com>
2025-07-09 10:17:23 +0530

    Refactor palindrome check in Linked List solution to use two-pointer technique

--- Python/234. Palindrome Linked List.py
+++ Python/234. Palindrome Linked List.py
@@ -18,3 +18,37 @@ class Solution:
             l+=1
             r-=1
         return True 
+
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def isPalindrome(self, head: Optional[ListNode]) -> bool:
+        #two pointer - fast and slow
+        fast = head
+        slow = head
+
+        while fast and fast.next:
+            fast = fast.next.next
+            slow = slow.next
+        #fast will be at end, slow will be at middle
+
+        #reverse 2dn half
+        prev = None
+        while slow:
+            temp = slow.next
+            slow.next=prev
+            prev= slow
+            slow = temp
+
+        #check for palindrom
+        l,r = head,prev
+
+        while r:
+            if (l.val != r.val):
+                return False
+            l = l.next
+            r = r.next
+        return True
