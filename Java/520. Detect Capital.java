332159a0a85b3599ca5f0753884077668907a1d9
mrnithesh <mr.nithesh.k@gmail.com>
2025-07-11 22:07:31 +0530

    Add solution for 520. Detect Capital problem - Java

---
+++ Java/520. Detect Capital.java
@@ -0,0 +1,23 @@
+class Solution {
+    public boolean detectCapitalUse(String word) {
+        int count = 0;
+        int n = word.length();
+        for (int i=0;i<n;i++){
+            if (Character.isUpperCase(word.charAt(i))){
+                count++;
+            }
+        }
+        if(count==0){
+            return true;
+        }
+        else if(count == n){
+            return true;
+        }
+        else if(count==1 && Character.isUpperCase(word.charAt(0))){
+            return true;
+        }
+        else{
+            return false;
+        }
+    }
+}
