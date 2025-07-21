444901c55c4dc40ecae7424b0ae8246fb9b2d9ba
mrnithesh <mr.nithesh.k@gmail.com>
2025-07-21 21:58:11 +0530

    Add solution for 1957. Delete Characters to Make Fancy String problem - Python

---
+++ Python/1957. Delete Characters to Make Fancy String.py
@@ -0,0 +1,8 @@
+class Solution:
+    def makeFancyString(self, s: str) -> str:
+        res = []
+        for c in s:
+            if len(res)>=2 and res[-1] == c and res[-2] == c:
+                continue
+            res.append(c)
+        return "".join(res)
