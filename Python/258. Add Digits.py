ef16fb17443fddecc226a9428d11a13d6abf1e4b
mrnithesh <mr.nithesh.k@gmail.com>
2025-07-22 11:33:34 +0530

    Add solution for 258. Add Digits problem - Python

---
+++ Python/258. Add Digits.py
@@ -0,0 +1,11 @@
+class Solution:
+    def addDigits(self, num: int) -> int:
+        while num>=10:
+            n = 0
+            while num>0:
+                digit = num % 10
+                n += digit
+                #print(digit,n)
+                num//=10
+            num=n
+        return num
