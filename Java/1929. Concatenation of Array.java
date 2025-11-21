375757132a2589d39de2673464cf4417e3e280af
mrnithesh <mr.nithesh.k@gmail.com>
2025-11-21 12:50:18 +0530

    Add solution for 1929. Concatenation of Array problem - Java and Python

---
+++ Java/1929. Concatenation of Array.java
@@ -0,0 +1,9 @@
+class Solution {
+    public int[] getConcatenation(int[] nums) {
+        int n = nums.length;
+        int [] ans = new int[n+n];
+        for (int i=0;i<n;i++) ans[i] = nums[i];
+        for (int i=0;i<n;i++) ans[i+n] = nums[i];
+        return ans;
+    }
+}
