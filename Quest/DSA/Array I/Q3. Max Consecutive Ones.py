69656e4be8a3e70226345b4ebda681bb1baf9719
mrnithesh <mr.nithesh.k@gmail.com>
2025-11-21 12:48:22 +0530

    Add solution for 485. Max Consecutive Ones problem - Python

---
+++ Quest/DSA/Array I/Q3. Max Consecutive Ones.py
@@ -0,0 +1,12 @@
+class Solution:
+    def findMaxConsecutiveOnes(self, nums: List[int]) -> int:
+        res = 0
+        curr = 0
+        for i in range(len(nums)):
+            if nums[i] == 0:
+                curr = 0
+                continue
+            if (nums[i] == 1):
+                curr+=1
+                res = max(res,curr)
+        return res
