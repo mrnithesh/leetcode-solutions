bec0cf9be312d1fc0e4d3e08137443aae5184632
mrnithesh <mr.nithesh.k@gmail.com>
2025-12-14 20:11:42 +0530

    Add optimized solution for 438. Find All Anagrams in a String problem using prefix sum - Java

--- Java/438. Find All Anagrams in a String.java
+++ Java/438. Find All Anagrams in a String.java
@@ -19,3 +19,40 @@ class Solution {
         return res;
     }
 }
+//faster and better approach using prefix sum
+class Solution {
+    public List<Integer> findAnagrams(String s, String p) {
+        List<Integer> res = new ArrayList<>();
+
+        if (s.length() < p.length()) {
+            return res;
+        }
+
+        int[] freqP = new int[26];
+        int[] freqWindow = new int[26];
+
+        for (char c : p.toCharArray()) {
+            freqP[c - 'a']++;
+        }
+
+        int windowSize = p.length();
+
+        for (int i = 0; i < windowSize; i++) {
+            freqWindow[s.charAt(i) - 'a']++;
+        }
+
+        if (Arrays.equals(freqP, freqWindow)) {
+            res.add(0);
+        }
+
+        for (int i = windowSize; i < s.length(); i++) {
+            freqWindow[s.charAt(i) - 'a']++;
+            freqWindow[s.charAt(i - windowSize) - 'a']--;
+            if (Arrays.equals(freqP, freqWindow)) {
+                res.add(i - windowSize + 1);
+            }
+        }
+
+        return res;
+    }
+}
